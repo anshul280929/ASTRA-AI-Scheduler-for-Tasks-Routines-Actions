c56a1035af525b98ae8987e1acf5e28cbdac5be2
Anshul <anshulbhaskar50@gmail.com>
2025-08-06 11:57:48 +0530

    Session Middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
 
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
+  const router= useRouter();
+  const {data,isLoading}= useCurrent();
+  const {mutate} =useLogout();
+
+  useEffect(()=>{
+    if(!data&&!isLoading){
+      router.push("/sign-in")
+    }
+  })
   return (
     <div>
-      
-      <Button >Click me</Button>
-      <Button variant="destructive">Delete</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="teritrary">Tertiary</Button>
+      Only visible to authorized users
+      <Button onClick={()=> mutate()}>
+        Logout
+      </Button>
     </div>
   )
 }
